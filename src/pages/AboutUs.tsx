e8ec25a271a29d8b2801d5aab2e08c95dadef574
DiegoDefSan <defsan35@gmail.com>
2023-10-23 14:16:20 -0300

    feat: pages added - implemented with header and footer

---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+export const AboutUs = () => {
+  return (
+    <div>
+      <Header activeLink="about-us" />
+      AboutUs
+      <Footer />
+    </div>
+  )
+};
---
+++ src/pages/Regions.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+export const Regions = () => {
+  return (
+    <div>
+      <Header activeLink="regions" />
+      Regions
+      <Footer />
+    </div>
+  )
+};
