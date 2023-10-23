4e0288f271eec0fe3ced7284f7dd02ada559435e
DiegoDefSan <defsan35@gmail.com>
2023-10-23 14:14:50 -0300

    feat: header added - tsx and styles

---
+++ src/components/Header.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import '../assets/styles/components/header.css';
+import { Link } from "react-router-dom";
+
+interface Props {
+  activeLink: string;
+}
+
+export const Header = ({ activeLink }: Props) => {
+
+  return (
+    <header>
+      <nav>
+        <div className="logo">
+          <a href="#"><img src="./images/logo-navbar.png" alt="Logo de Hecho en Perú" /></a>
+        </div>
+        <div className="nav-responsive">
+          <div className="menu d-flex justify-content-around">
+            <Link className={`menu-item ${activeLink === 'home' && 'active'}`} to="/">Home</Link>
+            <Link className={`menu-item ${activeLink === 'catalog' && 'active'}`} to="/catalog">Catalog</Link>
+            <Link className={`menu-item ${activeLink === 'regions' && 'active'}`} to="/regions">Regions</Link>
+            <Link className={`menu-item ${activeLink === 'about-us' && 'active'}`} to="/about-us">About us</Link>
+            <Link className={`menu-item ${activeLink === 'contact-us' && 'active'}`} to="/contact-us">Contact us</Link>
+          </div>
+          <div className="search-bar">
+            <input type="text" placeholder="Enter your category" />
+            <button type="submit">Search</button>
+          </div>
+        </div>
+        <div className="icons d-flex justify-content-around">
+
+          <i id="cart-icon" className="fas fa-shopping-cart"></i>
+
+          <a href="./public/login.html"><i className="fas fa-user"></i></a>
+          <i className="fa-solid fa-bars hamburger inactive"></i>
+        </div>
+      </nav>
+    </header >
+  )
+};
