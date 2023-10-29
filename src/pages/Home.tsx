cccb9e498b97e5abb1305661db9b0f1afc1262a0
DiegoDefSan <defsan35@gmail.com>
2023-10-29 11:40:39 -0300

    fix: button re localization

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { Hero } from "../components/Hero";
-import { PrimaryButton } from "../components/buttons/PrimaryButton";
+import { PrimaryButton } from "../components/buttons/impl/PrimaryButton";
 
 export const Home = () => {
   return (
