6905db5cb188e32ed38900d703607c3f87461a1d
DiegoDefSan <defsan35@gmail.com>
2023-10-31 17:55:11 -0300

    feat: price range filter component added

---
+++ src/components/filters/PriceRangeFilterComponent.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+interface Props {
+  minPrice: number;
+  maxPrice: number;
+  handleChange: Function;
+}
+
+export const PriceRangeFilterComponent = (props: Props) => {
+
+  const [priceRange, setPriceRange] = React.useState({
+    minPrice: props.minPrice,
+    maxPrice: props.maxPrice,
+  });
+
+  function handlePriceRangeChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setPriceRange({
+      ...priceRange,
+      [name]: value !== '' ? parseInt(value) : 0,
+    });
+
+
+    props.handleChange(priceRange);
+  }
+
+  return (
+    <fieldset className="fieldset-container" >
+      <legend>Price range</legend>
+
+      <div className="options-container" id='price-filter'>
+        <div className="number-input-container">
+          <span>Min</span>
+          <input
+            type="number"
+            placeholder="Min price"
+            name='minPrice'
+            value={priceRange.minPrice}
+            onChange={handlePriceRangeChange}
+          />
+        </div>
+        <br />
+        <div className="number-input-container">
+          <span>Max</span>
+          <input
+            type="number"
+            placeholder="Max price"
+            name='maxPrice'
+            value={priceRange.maxPrice}
+            onChange={handlePriceRangeChange}
+          />
+        </div>
+      </div>
+
+    </fieldset>
+  )
+}
