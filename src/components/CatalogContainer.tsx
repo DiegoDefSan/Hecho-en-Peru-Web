905ba6ac5f044ec49dce72ed4ac71e8a8b3475a0
DiegoDefSan <defsan35@gmail.com>
2023-11-03 13:39:27 -0300

    perf: filterProducts refactored

--- src/components/CatalogContainer.tsx
+++ src/components/CatalogContainer.tsx
@@ -30,46 +30,45 @@ export const CatalogContainer = (props: ApiProps) => {
       )),
   });
 
-  function filterProducts() {
-
-    const priceRangeSelected = filterData.priceRange;
-    const minPriceSelected = priceRangeSelected === "all" ? 0 : Number(filterData.priceRange.split("-")[0]);
-    const maxPriceSelected = priceRangeSelected === "all" ? 1000 : Number(filterData.priceRange.split("-")[1]);
-    const categoriesSelected = filterData.categories;
-    const regionsSelected = filterData.regions;
-
-    const filteredProducts = props.products
-      .filter((product) => {
-        const productPrice = product.price;
-        return productPrice >= minPriceSelected && productPrice <= maxPriceSelected;
-      })
-      .filter((product) => {
-        const productIdCategory = product.category.idCategory;
-        return categoriesSelected.find((category) => category.key === productIdCategory)?.checked;
-      })
-      .filter((product) => {
-        const productIdRegion = product.region.idRegion;
-        return regionsSelected.find((region) => region.key === productIdRegion)?.checked;
-      })
-      .sort((productA, productB) => {
-        switch (filterData.sortBy) {
-          case "price-asc":
-            return productA.price - productB.price;
-          case "price-desc":
-            return productB.price - productA.price;
-          case "rating-asc":
-            return productA.rating - productB.rating;
-          case "rating-desc":
-            return productB.rating - productA.rating;
-          default:
-            return 0;
-        }
-      });
-
-    setProducts(filteredProducts);
-  }
-
   React.useEffect(() => {
+    const filterProducts = () => {
+      const priceRangeSelected = filterData.priceRange;
+      const minPriceSelected = priceRangeSelected === "all" ? 0 : Number(filterData.priceRange.split("-")[0]);
+      const maxPriceSelected = priceRangeSelected === "all" ? 1000 : Number(filterData.priceRange.split("-")[1]);
+      const categoriesSelected = filterData.categories;
+      const regionsSelected = filterData.regions;
+
+      const filteredProducts = props.products
+        .filter((product) => {
+          const productPrice = product.price;
+          return productPrice >= minPriceSelected && productPrice <= maxPriceSelected;
+        })
+        .filter((product) => {
+          const productIdCategory = product.category.idCategory;
+          return categoriesSelected.find((category) => category.key === productIdCategory)?.checked;
+        })
+        .filter((product) => {
+          const productIdRegion = product.region.idRegion;
+          return regionsSelected.find((region) => region.key === productIdRegion)?.checked;
+        })
+        .sort((productA, productB) => {
+          switch (filterData.sortBy) {
+            case "price-asc":
+              return productA.price - productB.price;
+            case "price-desc":
+              return productB.price - productA.price;
+            case "rating-asc":
+              return productA.rating - productB.rating;
+            case "rating-desc":
+              return productB.rating - productA.rating;
+            default:
+              return 0;
+          }
+        });
+
+      setProducts(filteredProducts);
+    }
+
     filterProducts();
   }, [filterData]);
 
