c896306410e3628c522c3f2d9020494001fe19e9
DiegoDefSan <defsan35@gmail.com>
2023-11-03 09:26:49 -0300

    fix: handle change implementation fixed

--- src/components/filters/PriceRangeFilterComponent.tsx
+++ src/components/filters/PriceRangeFilterComponent.tsx
@@ -23,7 +23,10 @@ export const PriceRangeFilterComponent = (props: Props) => {
     });
 
 
-    props.handleChange(priceRange);
+    props.handleChange(
+      priceRange.minPrice,
+      priceRange.maxPrice,
+    );
   }
 
   return (
