16e1db2081480495e6945bd4362613422d9bdad6
DiegoDefSan <defsan35@gmail.com>
2023-12-12 19:17:03 -0300

    fix: name path fixed

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,8 +1,8 @@
 export const BASE_URL = "http://localhost:8080";
 export const GET_PRODUCTS_PATH = "/products";
-export const ADD_PRODUCT_PATH = "/products/addProduct";
-export const REMOVE_PRODUCT_PATH = "/products/deleteProduct";
-export const UPDATE_PRODUCT_PATH = "/products/update";
+export const POST_PRODUCT_PATH = "/products/addProduct";
+export const DELETE_PRODUCT_PATH = "/products/deleteProduct";
+export const PUT_PRODUCT_PATH = "/products/update";
 export const GET_CATEGORIES_PATH = "/categories";
 export const GET_HANDCRAFTS_PATH = "/handcrafts";
 export const GET_REGIONS_PATH = "/regions";
