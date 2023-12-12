c937c46ac5a1170e177c1b796426b188d52faaeb
DiegoDefSan <defsan35@gmail.com>
2023-12-12 09:55:52 -0300

    ref: useProducts refactored with useApi

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import Product from "../interfaces/product";
-import axios from "axios";
+import useApi from "./useApi";
+import { GET_PRODUCTS_PATH } from "../util/constants";
 
-const useProducts = () => useQuery({
-  queryKey: ["products"],
-  queryFn: () => axios
-    .get<Product[]>("http://localhost:8080/products")
-    .then(res => res.data),
-  staleTime: 10_000, // 10 seconds
-});
+const useProducts = () => {
+  const { getObjects } = useApi<Product>(GET_PRODUCTS_PATH);
+  return useQuery({
+    queryKey: ["products"],
+    queryFn: () => getObjects(),
+    staleTime: 10_000, // 10 seconds
+  });
+};
 
 export default useProducts;
