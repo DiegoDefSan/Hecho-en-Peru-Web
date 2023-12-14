2410f537ee416a794209463c1d38497e8cf5442a
DiegoDefSan <defsan35@gmail.com>
2023-12-14 09:48:44 -0300

    fix: delete product path modified

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = "http://localhost:8080";
 export const GET_PRODUCTS_PATH = "/products";
 export const POST_PRODUCT_PATH = "/products/addProduct";
-export const DELETE_PRODUCT_PATH = "/products/deleteProduct";
+export const DELETE_PRODUCT_PATH = "/products/delete";
 export const PUT_PRODUCT_PATH = "/products/update";
 export const GET_CATEGORIES_PATH = "/categories";
 export const GET_HANDCRAFTS_PATH = "/handcrafts";
