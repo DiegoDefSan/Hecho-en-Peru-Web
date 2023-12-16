83b9128695aa6c9b895983220914edb2f8283995
DiegoDefSan <defsan35@gmail.com>
2023-12-16 13:54:26 -0300

    fix: scroll to start tin new product page

--- src/pages/NewProductPage.tsx
+++ src/pages/NewProductPage.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { Footer } from "../components/Footer"
 import { Header } from "../components/Header"
 import { NewProductForm } from "../components/NewProductForm"
 
 export const NewProductPage = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+
   return (
     <div>
       <Header activeLink="" />
